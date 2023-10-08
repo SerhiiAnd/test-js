ac0f292c22c01ffb57d57419f32762aebf952503
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2023-10-08 22:19:33 +0200

    1234

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// const isComing = confirm("Please confirm hotel reservation");
-// console.log(isComing);
-
-// const hotelName = prompt("Please enter desired hotel name");
-// console.log(hotelName);
-
-// const value = prompt("Please enter a number!");
-// console.log(typeof value); // "string"
-// console.log(value); // "5"
