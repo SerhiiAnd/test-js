36337f7c4dd038fbf91ef853cf62d7ba6ed55f59
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2023-10-16 19:00:34 +0200

    1234

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(5 > 3);
+// console.log(5 > 3);
+
+// const year = 5;
+
+// if (year > 4) {
+//   console.log(5 > 4);
+// } else if (year > 8) {
+//   console.log(5 > 8);
+// } else if (year > 5) {
+//   console.log(5 > 5);
+// } else {
+//   console.log("else");
+// }
